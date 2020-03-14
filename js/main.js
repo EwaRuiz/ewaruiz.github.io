10ec21231f1e7e810d29ddac98a9f2875022310b
Edward <edward.g.ruiz@gmail.com>
2020-03-14 14:56:41 -0500

    add changes

--- js/main.js
+++ js/main.js
@@ -5,14 +5,3 @@ function onClickMenu(){
 
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 };
-
-Let progress = document.getElementById('progressbar'
-);
-Let totalHeight = document.body.scrollHeight -
-	window.innerHeight;
-window.onscroll = function() {
-	Let progressHeight = (window.pageYOFFset /
-		totalHeight) * 100;
-	progress.style.height = progressHeight * "%";
-
-};
